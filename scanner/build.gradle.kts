5d90afc194f49a7f15a5ac4d37a0ed570785f680
Jithu <jithukrishnaju@gmail.com>
2025-06-03 14:21:12 +0530

    added new module

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -1,20 +1,18 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
-    namespace = "com.jk.positrarxcustomer"
+    namespace = "com.jk.positraRx.scanner"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.jk.positrarxcustomer"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -35,6 +33,16 @@ android {
     }
 }
 
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -45,6 +53,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation(project(":customerCallTracker"))
-    implementation(project(":scanner"))
 }
---
+++ scanner/src/main/java/com/jk/positraRx/scanner/Test.kt
@@ -0,0 +1,3 @@
+package com.jk.positraRx.scanner
+
+data class Test(val name: String, val age: Int)
